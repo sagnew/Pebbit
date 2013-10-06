475b9228b276c24191c0777cad688e41376766a4
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-06 00:07:34 -0400

    Added corresponding reddit functions

--- reddit_functions.py
+++ reddit_functions.py
@@ -3,13 +3,33 @@ import praw
 
 r = praw.Reddit('Testing')
 
-def get_submission_dict():
+submission_dict = {}
+comment_dict = {}
+
+def build_dictionaries():
+    """
+    comment_dict[id][0] = submission title
+    comment_dict[id[[1] = array of comments
+    """
     submissions = r.get_subreddit('all')
 
-    reddit_dict = {}
-    for submission in submissions.get_top(limit=10):
-        reddit_dict[submission.title] = []
+    count = 0
+    for submission in submissions.get_top(limit=5):
+        submission_dict[count] = submission.title
+        comment_dict[count] = {}
+        comment_dict[count][0] = submission.title
+        comment_dict[count][1] = []
         for comment in submission.comments:
-            reddit_dict[submission.title].append(comment.__str__())
+            comment_dict[count]['comments'].append(comment.__str__())
+        count += 1
 
-    return reddit_dict
+def get_submission_dict():
+    return submission_dict
+
+def get_comments_by_submission_id(id):
+    return comment_dict[id]
+
+def get_comment_dict():
+    return comment_dict
+
+build_dictionaries()
