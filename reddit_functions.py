1f842bd30634f4284b6e7984667217db563e6951
Sam Agnew <sagnew@eden.rutgers.edu>
2013-10-07 23:18:53 -0400

    Organized everything into one class

--- reddit_functions.py
+++ reddit_functions.py
@@ -1,78 +1,71 @@
 import requests
 import praw
 
-r = praw.Reddit('Testing')
-
-submission_dict = {}
-comment_dict = {}
-initial_data = {}
-count_dict = {}
-
-def cache_initial_data():
-    submissions = r.get_subreddit('all')
-    initial_data['submissions'] = {}
-    initial_data['comments'] = {}
-
-    count = 0
-    for submission in submissions.get_top(limit=5):
-        initial_data['submissions'][count] = submission.title
-        initial_data['comments'][count] = {}
-        initial_data['comments'][count][0] = submission.title
-        initial_data['comments'][count][1] = []
-        comment_count = 0
-        for comment in submission.comments:
-            if comment_count > 10:
-                break
-            initial_data['comments'][count][1].append(comment.__str__())
-            comment_count += 1
-        count += 1
+class RedditRetriever():
 
 
-def build_dictionaries(pebble):
-    """
-    comment_dict[id][0] = submission title
-    comment_dict[id[[1] = array of comments
-    """
+    def __init__(self):
+        self.submission_dict = {}
+        self.comment_dict = {}
+        self.initial_data = {}
+        self.count_dict = {}
 
-    if pebble not in comment_dict.keys():
-        comment_dict[pebble] = {}
-    comment_dict[pebble] = initial_data['comments']
-    if pebble not in submission_dict.keys():
-        submission_dict[pebble] = {}
-    submission_dict[pebble] = initial_data['submissions']
+    def cache_initial_data(self):
+        r = praw.Reddit('Testing')
+        submissions = r.get_subreddit('all')
+        self.initial_data['submissions'] = {}
+        self.initial_data['comments'] = {}
 
-def get_submission_dict(pebble):
-    if pebble not in count_dict.keys():
-        count_dict[pebble] = {}
-        count_dict[pebble]['submissions'] = 0
-        count_dict[pebble]['comments'] = {}
-    if pebble not in submission_dict.keys():
-        submission_dict[pebble] = {}
-        build_dictionaries(pebble)
-    submission_count = count_dict[pebble]['submissions']
-    count_dict[pebble]['submissions'] += 1
-    return {0: submission_dict[pebble][submission_count]}
-
-def get_comments_by_submission_id(pebble, id):
-    if pebble not in count_dict.keys():
-        count_dict[pebble] = {}
-        count_dict[pebble]['submissions'] = 0
-        count_dict[pebble]['comments'] = {}
-    if pebble not in comment_dict.keys():
-        comment_dict[pebble] = {}
-        build_dictionaries(pebble)
-    if id not in count_dict[pebble]['comments'].keys():
-        count_dict[pebble]['comments'][id] = 0
-    comment_count = count_dict[pebble]['comments'][id]
-    count_dict[pebble]['comments'][id] += 1
-    return {0: comment_dict[pebble][id][1][comment_count]}
-
-def reset():
-    submission_dict = {}
-    comment_dict = {}
-    initial_data = {}
-    count_dict = {}
-    cache_initial_data()
+        count = 0
+        for submission in submissions.get_top(limit=25):
+            self.initial_data['submissions'][count] = submission.title
+            self.initial_data['comments'][count] = {}
+            self.initial_data['comments'][count][0] = submission.title
+            self.initial_data['comments'][count][1] = []
+            comment_count = 0
+            for comment in submission.comments:
+                if comment_count > 10:
+                    break
+                self.initial_data['comments'][count][1].append(comment.__str__())
+                comment_count += 1
+            count += 1
 
 
-cache_initial_data()
+    def build_dictionaries(self, pebble):
+        """
+        comment_dict[id][0] = submission title
+        comment_dict[id[[1] = array of comments
+        """
+
+        if pebble not in self.comment_dict.keys():
+            self.comment_dict[pebble] = {}
+        self.comment_dict[pebble] = self.initial_data['comments']
+        if pebble not in self.submission_dict.keys():
+            self.submission_dict[pebble] = {}
+        self.submission_dict[pebble] = self.initial_data['submissions']
+
+    def get_submission_dict(self, pebble):
+        if pebble not in self.count_dict.keys():
+            self.count_dict[pebble] = {}
+            self.count_dict[pebble]['submissions'] = 0
+            self.count_dict[pebble]['comments'] = {}
+        if pebble not in self.submission_dict.keys():
+            self.submission_dict[pebble] = {}
+            self.build_dictionaries(pebble)
+        submission_count = self.count_dict[pebble]['submissions']
+        self.count_dict[pebble]['submissions'] += 1
+        return {0: self.submission_dict[pebble][submission_count]}
+
+    def get_comments_by_submission_id(self, pebble, id):
+        if pebble not in self.count_dict.keys():
+            self.count_dict[pebble] = {}
+            self.count_dict[pebble]['submissions'] = 0
+            self.count_dict[pebble]['comments'] = {}
+        if pebble not in self.comment_dict.keys():
+            self.comment_dict[pebble] = {}
+            self.build_dictionaries(pebble)
+        if id not in self.count_dict[pebble]['comments'].keys():
+            self.count_dict[pebble]['comments'][id] = 0
+        comment_count = self.count_dict[pebble]['comments'][id]
+        self.count_dict[pebble]['comments'][id] += 1
+        return {0: self.comment_dict[pebble][id][1][comment_count]}
